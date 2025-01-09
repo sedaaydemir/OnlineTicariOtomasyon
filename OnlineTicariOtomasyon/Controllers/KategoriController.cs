ee23422926c8f5864ac19c43bef89adb7618dfdb
Seda Aydemir <seda.aydemir7@gmail.com>
2025-01-09 23:09:15 +0300

    ****

--- OnlineTicariOtomasyon/Controllers/KategoriController.cs
+++ OnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Siniflar;
 
 namespace OnlineTicariOtomasyon.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        Context c = new Context();
         public ActionResult Index()
+        {
+            var degerler =c.Kategoris.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult KategoriEkle() 
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
